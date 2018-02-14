c03f8c5d9b36743b2c823b28e365da5673a99edc
WINNIE-MANGUTHA <winnibenson@gmail.com>
2018-02-14 01:36:53 -0800

    first commit

---
+++ src/MyClass.java
@@ -0,0 +1,13 @@
+
+public class MyClass {
+
+   
+    public static void main(String[] args) {
+        int x , y , answer;
+        x = 78;
+        y = 35;
+        answer = x % y; 
+        System.out.println("answer =" + answer);
+    }
+    
+}
